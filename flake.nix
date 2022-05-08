1db16345491bdd80a1fde579a61cb4019c7b6900
Nick Ballard <znhihgiasy@gmail.com>
2022-05-08 16:06:13 +0800

    Add arma3-unix-launcher.

--- flake.nix
+++ flake.nix
@@ -86,6 +86,30 @@
             cargoPatches = [ ./armake2.patch ];
           };
 
+        arma3-unix-launcher =
+          with nixpkgs.legacyPackages."${system}";
+          if system != "x86_64-linux" then null else
+          let aulimg = builtins.fetchurl {
+            url = "https://github.com/muttleyxd/arma3-unix-launcher/releases/download/commit-355/Arma_3_Unix_Launcher-x86_64.AppImage";
+            sha256 = "sha256-Q82d1aBqufJErSTudvBUe4rxThrNE4Xydd7YrG9HcaU=";
+          }; in
+          stdenv.mkDerivation {
+            pname = "arma3-unix-launcher";
+            version = "6.6.6";
+            nativeBuildInputs = [ makeWrapper ];
+            dontUnpack = true;
+            dontConfigure = true;
+            dontBuild = true;
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/bin
+              mkdir -p $out/share/arma3-unix-launcher
+              cp ${aulimg} $out/share/arma3-unix-launcher/launcher.AppImage
+              makeWrapper ${appimage-run}/bin/appimage-run $out/bin/arma3-unix-launcher --add-flags "$out/share/arma3-unix-launcher/launcher.AppImage"
+              runHook postInstall
+            '';
+          };
+
       };
     });
 }
