98a80dcf90d0299a8f0b505fdd9f2143281d55fa
Nick Ballard <znhihgiasy@gmail.com>
2023-01-23 12:43:11 +0800

    Mongo.db was empty, filled now with good stuff

--- flake.nix
+++ flake.nix
@@ -151,6 +151,29 @@
             '';
           };
 
+        rust-async-book =
+          with nixpkgs.legacyPackages."${system}";
+          stdenv.mkDerivation {
+            pname = "rust-async-book";
+            version = "6.6.6";
+            src = fetchFromGitHub {
+              owner = "rust-lang";
+              repo = "async-book";
+              rev = "e224ead5275545acc00fa82d94ba6d6f377c8563";
+              sha256 = "sha256-lzUljczQ7hPZAVi+bFXth0sDTX+C/W1f/iBiK3gPtO0=";
+            };
+            dontConfigure = true;
+            dontBuild = true;
+            nativeBuildInputs = [ mdbook-epub ];
+            installPhase = ''
+              runHook preInstall
+              mkdir $out
+              mdbook-epub --standalone
+              mv book $out/
+              runHook postInstall
+            '';
+          };
+
         chatterbot =
           # with (import nixos1909 { inherit system; });
           with nixpkgs.legacyPackages."${system}";
@@ -219,7 +242,7 @@
 
         rtw89 =
           with nixpkgs.legacyPackages."${system}";
-          let kernel = linuxKernel.packages.linux_6_0.kernel;
+          let kernel = linuxKernel.packages.linux_6_1.kernel;
               modDestDir = "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/realtek/rtw89";
           in
           stdenv.mkDerivation {
