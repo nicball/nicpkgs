91f796da38147f1870b44086ce339518731bc92d
Nick Ballard <znhihgiasy@gmail.com>
2023-02-11 19:20:34 +0800

    I hate this fucking language.

--- lib.nix
+++ lib.nix
@@ -1,9 +1,15 @@
 { pkgs }:
 {
-    modifyDerivationOutput = drv: args:
+    modifyDerivationOutput =
+        drv:
+        { pname
+        , version
+        , buildInputs ? []
+        , nativeBuildInputs ? []
+        , installPhase ? "runHook preInstall; runHook postInstall"
+        }@args:
         pkgs.stdenv.mkDerivation {
-            inherit (args)
-                pname version buildInputs nativeBuildInputs;
+            inherit pname version buildInputs nativeBuildInputs;
             installPhase = ''
                 cp -r ${drv} $out
                 chmod -R +w $out
