ae757b3c3f77db4c36de7041f6d8bd903ac001e7
Nick Ballard <znhihgiasy@gmail.com>
2023-02-13 21:47:42 +0800

    Add wayland-book.

--- flake.nix
+++ flake.nix
@@ -176,7 +176,29 @@
             '';
           };
 
-       thu-checkin =
+        wayland-book =
+          with nixpkgs.legacyPackages."${system}";
+          stdenv.mkDerivation {
+            pname = "wayland-book";
+            version = "6.6.6";
+            src = fetchgit {
+              url = "https://git.sr.ht/~sircmpwn/wayland-book";
+              rev = "9023e006f51a37b4ee84b42966aeeaa4f6fd44c5";
+              sha256 = "sha256-I8bhpzisH2veHdCtEBC06eIQrVZk8BDvWhb8JDdWiwg=";
+            };
+            dontConfigure = true;
+            dontBuild = true;
+            nativeBuildInputs = [ mdbook-epub ];
+            installPhase = ''
+              runHook preInstall
+              mkdir $out
+              mdbook-epub --standalone
+              mv book $out/
+              runHook postInstall
+            '';
+          };
+
+        thu-checkin =
           with nixpkgs.legacyPackages."${system}";
           let python = python3.withPackages (p: with p; [ requests pillow pytesseract ]); in
           stdenv.mkDerivation {
