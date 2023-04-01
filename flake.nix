64a793066b7780c081944c9f3884c19b168bfa81
Nick Ballard <znhihgiasy@gmail.com>
2023-04-01 14:58:58 +0800

    Make rtw89 overridable.

--- flake.nix
+++ flake.nix
@@ -227,32 +227,35 @@
         rtw89 =
           if system != "x86_64-linux" then null else
           with pkgs;
-          let kernel = linuxKernel.packages.linux_6_1.kernel;
-              modDestDir = "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/realtek/rtw89";
-          in
-          stdenv.mkDerivation {
-            pname = "rtw89";
-            version = "9.9.9";
-            src = fetchFromGitHub {
-              owner = "lwfinger";
-              repo = "rtw89";
-              rev = "bea0ed7298727397670505672316c49a68d59d80";
-              sha256 = "sha256-tHcjr7o3JAWzPm81cX4OxbkexDnDv0jK67hrBU+8svI=";
-            };
-            hardeningDisable = [ "pic" "format" ];
-            nativeBuildInputs = kernel.moduleBuildDependencies ++ [ openssl mokutil ];
-            makeFlags = kernel.makeFlags ++ [
-              "KVER=${kernel.modDirVersion}"
-              "KSRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-            ];
-            installPhase = ''
-              runHook preInstall
-              mkdir -p ${modDestDir}
-              find . -name '*.ko' -exec cp --parents {} ${modDestDir} \;
-              find ${modDestDir} -name '*.ko' -exec xz -f {} \;
-              runHook postInstall
-            '';
-          };
+          pkgs.lib.flip pkgs.lib.makeOverridable {} (
+            { kernel ? linux }:
+            let
+                modDestDir = "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/realtek/rtw89";
+            in
+            stdenv.mkDerivation {
+              pname = "rtw89";
+              version = "9.9.9";
+              src = fetchFromGitHub {
+                owner = "lwfinger";
+                repo = "rtw89";
+                rev = "bea0ed7298727397670505672316c49a68d59d80";
+                sha256 = "sha256-tHcjr7o3JAWzPm81cX4OxbkexDnDv0jK67hrBU+8svI=";
+              };
+              hardeningDisable = [ "pic" "format" ];
+              nativeBuildInputs = kernel.moduleBuildDependencies ++ [ openssl mokutil ];
+              makeFlags = kernel.makeFlags ++ [
+                "KVER=${kernel.modDirVersion}"
+                "KSRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+              ];
+              installPhase = ''
+                runHook preInstall
+                mkdir -p ${modDestDir}
+                find . -name '*.ko' -exec cp --parents {} ${modDestDir} \;
+                find ${modDestDir} -name '*.ko' -exec xz -f {} \;
+                runHook postInstall
+              '';
+            }
+          );
 
         wemeet =
           if system != "x86_64-linux" then null else
