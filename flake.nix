b495f17399e5cfdef7d70143a226ee2551d09f34
Nick Ballard <znhihgiasy@gmail.com>
2023-04-02 19:28:11 +0800

    Add kindle-tool.

--- flake.nix
+++ flake.nix
@@ -283,6 +283,26 @@
             buildInputs = [ stdenv.cc.cc.lib ];
             doCheck = false;
           };
+
+        kindle-tool =
+          with pkgs;
+          stdenv.mkDerivation {
+            pname = "KindleTool";
+            version = "1.6.5";
+            src = fetchFromGitHub {
+              owner = "NiLuJe";
+              repo = "KindleTool";
+              rev = "v1.6.5";
+              sha256 = "sha256-Io+tfwgRAPEx+TQKZLBGrrHGAVS6ndgOOh+KlBh4t2U=";
+            };
+            nativeBuildInputs = [ pkg-config ];
+            buildInputs = [ zlib libarchive nettle ];
+            makeFlags = [
+              "DESTDIR="
+              "PREFIX=$(out)"
+            ];
+          };
+
       };
     });
 }
