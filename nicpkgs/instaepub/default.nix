2965a410cd4340a80345f8263cdd4c7af246c144
Nick Ballard <znhihgiasy@gmail.com>
2023-04-29 14:57:58 +0800

    Fix adding those shit.

--- nicpkgs/instaepub/default.nix
+++ nicpkgs/instaepub/default.nix
@@ -5,22 +5,24 @@
 , pandoc
 }:
 
-lib.flip lib.makeOverridable {} (
-  { consumerKey ? "badkey"
-  , consumerSecret ? "verysecret"
-  , username ? "nobody"
-  , password ? "p@ssw0rd"
-  , outputDir ? "."
-  }:
-  let
-    src = substituteAll {
-      src = ./instaepub.py;
-      inherit consumerKey consumerSecret username password outputDir;
-      inherit pandoc;
-    };
-  in
-  writers.writePython3
-    "instaepub"
-    { libraries = with python3Packages; [ requests requests_oauthlib urllib3 ]; }
-    src
-)
+{ consumerKey ? "badkey"
+, consumerSecret ? "verysecret"
+, username ? "nobody"
+, password ? "p@ssw0rd"
+, outputDir ? "."
+}:
+
+let
+
+  src = substituteAll {
+    src = ./instaepub.py;
+    inherit consumerKey consumerSecret username password outputDir;
+    inherit pandoc;
+  };
+
+in
+
+writers.writePython3
+  "instaepub"
+  { libraries = with python3Packages; [ requests requests_oauthlib urllib3 ]; }
+  src
--- nicpkgs/screenshot/default.nix
+++ nicpkgs/screenshot/default.nix
@@ -5,37 +5,36 @@
 , imagemagick
 }:
 
-lib.flip lib.makeOverridable {} (
-  { scale ? "1"
-  , output-dir ? "~/screenshots"
-  , format ? "$(date +%Y-%m-%d_%H-%M-%S)"
-  }:
-  writeShellScriptBin "screenshot" ''
-    fullscreen=false
-    savetodisk=false
-    for i; do
-      case "$i" in
-        --full)
-          fullscreen=true
-          ;;
-        --save)
-          savetodisk=true
-          ;;
-      esac
-    done
-    if $fullscreen; then
-      ${grim}/bin/grim /tmp/latest-screenshot.png
-    else
-      file=$(mktemp /tmp/XXXXXXXXXXX-screenshot.png)
-      ${grim}/bin/grim $file
-      read w h x y < <(${slurp}/bin/slurp -f "%w %h %x %y")
-      ${imagemagick}/bin/convert $file -crop $((w*${scale}))x$((h*${scale}))+$((x*${scale}))+$((y*${scale})) /tmp/latest-screenshot.png
-      rm $file
-    fi
-    wl-copy --type image/png < /tmp/latest-screenshot.png
-    if $savetodisk; then
-      mkdir -p ${output-dir}
-      cp /tmp/latest-screenshot.png ${output-dir}/${format}.png
-    fi
-  ''
-)
+{ scale ? "1"
+, output-dir ? "~/screenshots"
+, format ? "$(date +%Y-%m-%d_%H-%M-%S)"
+}:
+
+writeShellScriptBin "screenshot" ''
+  fullscreen=false
+  savetodisk=false
+  for i; do
+    case "$i" in
+      --full)
+        fullscreen=true
+        ;;
+      --save)
+        savetodisk=true
+        ;;
+    esac
+  done
+  if $fullscreen; then
+    ${grim}/bin/grim /tmp/latest-screenshot.png
+  else
+    file=$(mktemp /tmp/XXXXXXXXXXX-screenshot.png)
+    ${grim}/bin/grim $file
+    read w h x y < <(${slurp}/bin/slurp -f "%w %h %x %y")
+    ${imagemagick}/bin/convert $file -crop $((w*${scale}))x$((h*${scale}))+$((x*${scale}))+$((y*${scale})) /tmp/latest-screenshot.png
+    rm $file
+  fi
+  wl-copy --type image/png < /tmp/latest-screenshot.png
+  if $savetodisk; then
+    mkdir -p ${output-dir}
+    cp /tmp/latest-screenshot.png ${output-dir}/${format}.png
+  fi
+''
