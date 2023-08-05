932467c075e0fbc2ccaa39b7bc176b5ba95e0188
Nick Ballard <znhihgiasy@gmail.com>
2023-08-05 20:18:06 +0800

    Add ryzenadj. Ditch solarized.

---
+++ nicpkgs/ryzenadj.nix
@@ -0,0 +1,22 @@
+{ lib, stdenv, fetchFromGitHub, pciutils, cmake }:
+
+stdenv.mkDerivation rec {
+  pname = "ryzenadj";
+  version = "snapshot";
+
+  src = fetchFromGitHub {
+    owner = "FlyGoat";
+    repo = "RyzenAdj";
+    rev = "7201ff25533698bfe3c402a87d479e02358bd929";
+    sha256 = "sha256-znBKCmZTUP2oOcOWKCr7LyZDm9u5257ia0wzrpVq0ps=";
+  };
+
+  nativeBuildInputs = [ pciutils cmake ];
+
+  installPhase = ''
+    install -D libryzenadj.so $out/lib/libryzenadj.so
+    install -D ryzenadj $out/bin/ryzenadj
+  '';
+
+  meta.platforms = [ "x86_64-linux" ];
+}
