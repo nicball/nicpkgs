5df0113fb4e0dffa0c45fa3af539be3b084f419f
Nick Ballard <znhihgiasy@gmail.com>
2023-10-02 17:42:54 +0800

    Upgrade ryzenadj.

--- nicpkgs/ryzenadj.nix
+++ nicpkgs/ryzenadj.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ryzenadj";
-  version = "unstable-2023-07-14";
+  version = "0.14.0";
 
   src = fetchFromGitHub {
     owner = "FlyGoat";
     repo = "RyzenAdj";
-    rev = "7201ff25533698bfe3c402a87d479e02358bd929";
-    sha256 = "sha256-znBKCmZTUP2oOcOWKCr7LyZDm9u5257ia0wzrpVq0ps=";
+    rev = "4b558e75a4810f8e13375c8c3ae08025fec276ac";
+    sha256 = "sha256-elIup39t1CssFFvfe7z/euUifGenWgpvdiogD4KixTA=";
   };
 
   nativeBuildInputs = [ pciutils cmake ];
