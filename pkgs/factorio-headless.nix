435bb320e94584b2fed35fff788f90ed55c87d94
Nick Ballard <znhihgiasy@gmail.com>
2024-02-17 16:14:42 +0800

    _

--- pkgs/factorio-headless.nix
+++ pkgs/factorio-headless.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   pname = "factorio-headless";
-  version = "1.1.101";
+  version = "1.1.104";
   src = fetchurl {
     name = "factorio_headless_x64-${version}.tar.xz";
     url = "https://factorio.com/get-download/${version}/headless/linux64";
-    sha256 = "sha256-Boim9hWoe/c9P0a1PcQj7jfK0/xEEnI/PXRQrtFjg5I=";
+    sha256 = "sha256-jhM1OrI9V5idt7BllEEdMIhd4akj86mJ0SdJwavAFYM=";
   };
   nativeBuildInputs = [ autoPatchelfHook ];
   preferLocalBuild = true;
