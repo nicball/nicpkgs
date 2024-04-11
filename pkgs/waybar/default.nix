b1893d946a5dad0b640031f1748df262dc05569f
Nick Ballard <znhihgiasy@gmail.com>
2024-04-11 18:17:44 +0800

    Never gonna say goodbye

--- pkgs/waybar/default.nix
+++ pkgs/waybar/default.nix
@@ -1,6 +1,22 @@
-{ wrapDerivationOutput, super }:
+{ lib, wrapDerivationOutput, fetchFromGitHub, super }:
 
-wrapDerivationOutput super.waybar "bin/waybar" ''
+let
+  waybar-old = (super.waybar.override {
+    pipewireSupport = false;
+    cavaSupport = false;
+  }).overrideAttrs (self: super: {
+    version = "0.9.24";
+    src = fetchFromGitHub {
+      owner = "Alexays";
+      repo = "waybar";
+      rev = "0.9.24";
+      sha256 = "sha256-JhLKGzqZ8akWcyHTav2TGcGmXk9dy9Xj4+/oFCPeNU0=";
+    };
+    mesonFlags = lib.remove "-Dpipewire=disabled" super.mesonFlags;
+  });
+in
+
+wrapDerivationOutput waybar-old "bin/waybar" ''
   --add-flags '--config ${./waybar-config}' \
   --add-flags '--style ${./waybar-style.css}'
 ''
