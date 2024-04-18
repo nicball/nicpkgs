8d1870a813563b637c272c032ac8d5d33d3c4336
Nick Ballard <znhihgiasy@gmail.com>
2024-04-18 16:02:31 +0800

    I must enjoy torturing myself

--- pkgs/wrap-derivation-output.nix
+++ pkgs/wrap-derivation-output.nix
@@ -14,34 +14,32 @@ let
 in
 
 {
-  fns = {
-    modifyDerivationOutput =
-      drv:
-      { buildInputs ? []
-      , nativeBuildInputs ? []
-      , extraCommands
-      , ...
-      }@args:
-      stdenv.mkDerivation (getDerivationName args // {
-        inherit buildInputs nativeBuildInputs;
-        dontUnpack = true;
-        installPhase = ''
-          runHook preInstall
-          mkdir $out
-          ${lndir}/bin/lndir -silent ${drv} $out
-          ${extraCommands}
-          runHook postInstall
-        '';
-        meta.platforms = drv.meta.platforms;
-      });
+  modifyDerivationOutput =
+    drv:
+    { buildInputs ? []
+    , nativeBuildInputs ? []
+    , extraCommands
+    , ...
+    }@args:
+    stdenv.mkDerivation (getDerivationName args // {
+      inherit buildInputs nativeBuildInputs;
+      dontUnpack = true;
+      installPhase = ''
+        runHook preInstall
+        mkdir $out
+        ${lndir}/bin/lndir -silent ${drv} $out
+        ${extraCommands}
+        runHook postInstall
+      '';
+      meta.platforms = drv.meta.platforms;
+    });
 
-    wrapDerivationOutput = drv: path: args:
-      self.modifyDerivationOutput drv (getDerivationName drv // {
-        nativeBuildInputs = [ makeWrapper ];
-        extraCommands = ''
-          mv $out/${path} $out/${path}-unwrapped
-          makeWrapper $out/${path}-unwrapped $out/${path} ${args}
-        '';
-      });
-  };
+  wrapDerivationOutput = drv: path: args:
+    self.modifyDerivationOutput drv (getDerivationName drv // {
+      nativeBuildInputs = [ makeWrapper ];
+      extraCommands = ''
+        mv $out/${path} $out/${path}-unwrapped
+        makeWrapper $out/${path}-unwrapped $out/${path} ${args}
+      '';
+    });
 }
