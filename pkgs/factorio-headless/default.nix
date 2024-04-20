c0a7beb924cbe1f6d6c70125becaee1f83746623
Nick Ballard <znhihgiasy@gmail.com>
2024-04-20 18:06:02 +0800

    bla

--- pkgs/factorio-headless/default.nix
+++ pkgs/factorio-headless/default.nix
@@ -2,11 +2,13 @@
 , fetchurl
 , autoPatchelfHook
 , lib
+, box64
 , system
+, makeWrapper
 }:
 
 let
-  release = (lib.importJSON ./versions.json).${system}.headless.stable;
+  release = (lib.importJSON ./versions.json).x86_64-linux.headless.stable;
 in
 
 stdenv.mkDerivation rec {
@@ -15,12 +17,16 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     inherit (release) name url sha256;
   };
-  nativeBuildInputs = [ autoPatchelfHook ];
+  nativeBuildInputs = [ autoPatchelfHook ] ++ lib.optionals (system == "aarch64-linux") [ makeWrapper ];
   preferLocalBuild = true;
   dontBuild = true;
   installPhase = ''
     mkdir -p $out/{bin,share/factorio}
     cp -a data $out/share/factorio
     cp -a bin/x64/factorio $out/bin/factorio
+    ${lib.optionalString (system == "aarch64-linux") ''
+      mv $out/bin/factorio $out/bin/.factorio-unwrapped
+      makeWrapper ${box64}/bin/box64 $out/bin/factorio --add-flags "$out/bin/.factorio-unwrapped"
+    ''}
   '';
 }
