f86afd9a06a8ddf4bf517ac390c426fce84f9030
Nick Ballard <znhihgiasy@gmail.com>
2024-05-04 19:29:43 +0800

    This should work until december 2013.

--- flake.nix
+++ flake.nix
@@ -26,12 +26,31 @@
         mypkgs = ps: builtins.intersectAttrs (overlay 42 42) ps;
 
         addEverything = system: pkgs: ps: ps // {
-          everything =
+          build-everything-unsubstitutable =
             let
               isBroken = lib.attrByPath [ "meta" "broken" ] false;
-              goodPkgs = (builtins.filter (v: !isBroken v && lib.isDerivation v && lib.meta.availableOn { inherit system; } v) (lib.attrValues ps));
-              pathList = builtins.map (drv: { inherit (drv) name; path = "${drv}"; }) goodPkgs;
-            in pkgs.linkFarm "everything" pathList;
+              isGood = p: lib.isDerivation p && !isBroken p && lib.meta.availableOn { inherit system; } p;
+              goodPkgs = lib.filterAttrs (k: v: isGood v) ps;
+              nameList = lib.concatMapStringsSep " " lib.escapeShellArg (lib.attrNames goodPkgs);
+            in
+            pkgs.writeShellApplication {
+              name = "build-everything-unsubstitutable";
+              runtimeInputs = with pkgs; [ jq nix ];
+              text = ''
+                bincache=''${1:?"Please tell me which binary cache to check"}
+                build=''${2:-"nix build .#"}
+                for name in ${nameList}; do
+                  path=$(nix path-info --json .#"$name" 2>/dev/null | jq -r '.[0].path')
+                  exist=$(nix path-info --json "$path" --store "$bincache" 2>/dev/null | jq '.[0].valid')
+                  if $exist; then
+                    echo "$name already exists, skipping..."
+                  else
+                    echo "Building $name..."
+                    eval "$build'$name'"
+                  fi
+                done
+              '';
+            };
         };
 
       in {
