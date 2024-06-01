2379f72a30c57408631c7153a624f195e7d39f41
Nick Ballard <znhihgiasy@gmail.com>
2024-06-01 16:46:05 +0800

    [no message]

--- pkgs/waybar/default.nix
+++ pkgs/waybar/default.nix
@@ -1,6 +1,6 @@
 { lib, wrapDerivationOutput, fetchFromGitHub, super }:
 
 wrapDerivationOutput super.waybar "bin/waybar" ''
-  --add-flags '--config ${./waybar-config}' \
-  --add-flags '--style ${./waybar-style.css}'
+  --add-flags '--config ${./config}' \
+  --add-flags '--style ${./style.css}'
 ''
