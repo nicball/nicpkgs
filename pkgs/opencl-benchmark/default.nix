5e782835e5312940076724811bcecc1d978fbbc7
Nick Ballard <znhihgiasy@gmail.com>
2024-09-30 20:22:48 +0800

    Add OpenCLBenchmark.

---
+++ pkgs/opencl-benchmark/default.nix
@@ -0,0 +1,18 @@
+{ lib, stdenv, fetchFromGitHub, opencl-headers, opencl-clhpp, ocl-icd, OpenCL }:
+
+stdenv.mkDerivation rec {
+  pname = "OpenCL-Benchmark";
+  version = "1.5";
+  src = fetchFromGitHub {
+    owner = "ProjectPhysX";
+    repo = "OpenCL-Benchmark";
+    rev = "v${version}";
+    sha256 = "sha256-urn4mNmfX4HbUc9b8RvClRCeAa0ETnRSmidricq4ad8=";
+  };
+  patches = [ ./cl.patch ];
+  buildInputs = [ opencl-clhpp ]
+    ++ lib.optionals stdenv.isDarwin [ OpenCL ]
+    ++ lib.optionals (!stdenv.isDarwin) [ opencl-headers ocl-icd ];
+  buildPhase = "g++ src/*.cpp -std=c++17 -pthread -O2 -lOpenCL -o OpenCL-Benchmark";
+  installPhase = "mkdir $out/bin -p; mv OpenCL-Benchmark $out/bin";
+}
--- pkgs/pandoc-static.nix
+++ pkgs/pandoc-static.nix
@@ -2,15 +2,21 @@
 
 let
 
-  pkgs = {
+  version = "3.1.9";
+
+  name = "pandoc-${version}";
+
+  drvs = {
 
     "x86_64-linux" = fetchzip {
-      url = "https://github.com/jgm/pandoc/releases/download/3.1.9/pandoc-3.1.9-linux-amd64.tar.gz";
+      inherit name;
+      url = "https://github.com/jgm/pandoc/releases/download/${version}/pandoc-${version}-linux-amd64.tar.gz";
       sha256 = "18jcjfq2wmxmy03f2vxf7i9ls9li7rmypdrdb1gh9c5ypkrn2g4r";
     };
 
     "aarch64-linux" = fetchzip {
-      url = "https://github.com/jgm/pandoc/releases/download/3.1.9/pandoc-3.1.9-linux-arm64.tar.gz";
+      inherit name;
+      url = "https://github.com/jgm/pandoc/releases/download/${version}/pandoc-${version}-linux-arm64.tar.gz";
       sha256 = "1cxcp5vya7bi19f0jkwrkxqv8ppmxipi8nzh4gknrqpmp7w6hx8b";
     };
 
@@ -18,6 +24,6 @@ let
 
 in
 
-pkgs.${system} // {
-  meta.platforms = builtins.attrNames pkgs;
+(drvs.${system} or {}) // {
+  meta.platforms = builtins.attrNames drvs;
 }
