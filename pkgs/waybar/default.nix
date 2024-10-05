413410b20517f811e6029b90915eee42597386f3
Nick Ballard <znhihgiasy@gmail.com>
2024-10-05 20:39:02 +0800

    Add niri support for waybar.

--- pkgs/waybar/default.nix
+++ pkgs/waybar/default.nix
@@ -1,6 +1,8 @@
-{ lib, wrapDerivationOutput, fetchFromGitHub, super }:
+{ lib, substituteAll, wrapDerivationOutput, fetchFromGitHub, super, wm ? "niri" }:
+
+let config = substituteAll { src = ./config; inherit wm; }; in
 
 wrapDerivationOutput super.waybar "bin/waybar" ''
-  --add-flags '--config ${./config}' \
+  --add-flags '--config ${config}' \
   --add-flags '--style ${./style.css}'
 ''
