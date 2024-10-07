4e6aac280235da932948c3f0ba2d0df21d9ab525
Nick Ballard <znhihgiasy@gmail.com>
2024-10-07 20:36:14 +0800

    Restructure modules.

---
+++ nixos-modules/default.nix
@@ -0,0 +1,28 @@
+{ overlay }:
+
+{ lib, config, ... }:
+
+{
+  imports = [
+    (import ./overlay.nix { inherit overlay; })
+    ./window-managers.nix
+    ./waybar.nix
+    ./greetd.nix
+    ./kitty.nix
+    ./dunst.nix
+    ./rofi-wayland.nix
+  ];
+
+  options.nic.set-nix-path = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+  };
+
+  config = lib.mkIf config.nic.set-nix-path {
+    nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
+    nixpkgs.flake = {
+      setFlakeRegistry = false;
+      setNixPath = false;
+    };
+  };
+}
