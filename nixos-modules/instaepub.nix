50b62c1531db68ae609d359c4b1ce6b862f269ee
Nick Ballard <znhihgiasy@gmail.com>
2024-10-24 16:15:59 +0800

    Add nixos counterparts of home modules.

---
+++ nixos-modules/instaepub.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, lib, ... }:
+
+let
+
+  cfg = config.nic.instaepub;
+
+  package = pkgs.instaepub.override {
+    inherit (cfg)
+      consumer-key
+      consumer-secret
+      username
+      password
+      output-dir
+      auto-archive
+      pandoc;
+  };
+
+in
+
+{
+
+  options.nic.instaepub = {
+
+    enable = lib.mkEnableOption "instaepub";
+
+    consumer-key = lib.mkOption { type = lib.types.str; };
+
+    consumer-secret = lib.mkOption { type = lib.types.str; };
+
+    username = lib.mkOption { type = lib.types.str; };
+
+    password = lib.mkOption { type = lib.types.str; };
+
+    output-dir = lib.mkOption { type = lib.types.str; };
+
+    auto-archive = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+
+    interval = lib.mkOption {
+      type = lib.types.str;
+      default = "daily";
+    };
+
+    pandoc = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.pandoc;
+    };
+
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    systemd.services.instaepub = {
+      description = "InstaEpub - Fetch your instapaper as epub";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${package}/bin/instaepub";
+      };
+    };
+
+    systemd.timers.instaepub = {
+      description = "InstaEpub - Fetch your instapaper as epub";
+      timerConfig.OnCalendar = cfg.interval;
+      wantedBy = [ "timers.target" ];
+    };
+
+  };
+
+}
