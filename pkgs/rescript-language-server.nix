ff03fbcef9a03e4974af3e31be4eb53820b46229
Nick Ballard <znhihgiasy@gmail.com>
2024-11-30 19:58:45 +0800

    Update rescript language server.

--- pkgs/rescript-language-server.nix
+++ pkgs/rescript-language-server.nix
@@ -5,13 +5,13 @@
 }:
 
 let
-  version = "1.50.0";
+  version = "1.58.0";
 
   source = fetchFromGitHub {
     owner = "rescript-lang";
     repo = "rescript-vscode";
     rev = version;
-    sha256 = "sha256-4b2Z94/CCvPge9qKmv8svUib8zJ9NEZ+FYeylgmkKBQ=";
+    sha256 = "sha256-v+qCVge57wvA97mtzbxAX9Fvi7ruo6ZyIC14O8uWl9Y=";
   };
 
   analysis = stdenv.mkDerivation {
@@ -35,7 +35,7 @@ buildNpmPackage rec {
   inherit version;
   src = source;
   sourceRoot = "${src.name}/server";
-  npmDepsHash = "sha256-xxGELwjKIGRK1/a8P7uvUCKrP9y8kqAHSBfi2/IsebU=";
+  npmDepsHash = "sha256-GXbYYtrNRbv/yl1U/171+I9+jwWDMA6ZA9lHvGAhM98=";
   postPatch = ''
     cp -r ${analysis}/bin analysis_binaries/linux
   '';
