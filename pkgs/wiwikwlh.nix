030c3f9ca48c9585722697f2c9d49a53bef02d82
Nick Ballard <znhihgiasy@gmail.com>
2024-12-01 11:40:52 +0800

    Typo.

--- pkgs/wiwikwlh.nix
+++ pkgs/wiwikwlh.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
   version = "unstable-2021-06-29";
   src = fetchFromGitHub {
     owner = "sdiehl";
-    repo = "wiwikwlh";
+    repo = "wiwinwlh";
     rev = "59ccf63de431074bd202805d888b56de2d0c8ebb";
     sha256 = "sha256-0z/ZWi42cRvlL7YNy7s68Lg8Ij23Z/ljUTnD7ePq/JM=";
   };
