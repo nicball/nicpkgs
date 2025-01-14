1b67fa656f7e28076b32929a15453dafb510cfb5
Nick Ballard <znhihgiasy@gmail.com>
2025-01-14 12:52:50 +0800

    Update lilypondbot.

--- pkgs/lilypondbot/default.nix
+++ pkgs/lilypondbot/default.nix
@@ -1,14 +1,14 @@
 { writeShellApplication
 , curl
 , jq
-, lilypond
+, lilypond-unstable
 , timidity
 , ffmpeg
 }:
 
 writeShellApplication {
   name = "lilypondbot";
-  runtimeInputs = [ curl jq lilypond timidity ffmpeg ];
+  runtimeInputs = [ curl jq lilypond-unstable timidity ffmpeg ];
   text = ''
     common=${./common.ly}
     ${builtins.readFile ./lilypondbot.sh}
