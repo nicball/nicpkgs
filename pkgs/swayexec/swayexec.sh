e4f52277c44241c42f904d7c733814d1475e6032
Nick Ballard <znhihgiasy@gmail.com>
2025-02-17 16:41:42 +0800

    Update swayexec.

--- pkgs/swayexec/swayexec.sh
+++ pkgs/swayexec/swayexec.sh
@@ -1,8 +1,8 @@
 cmd=()
 for i in "$@"; do
-  cmd=("${cmd[@]}" "$(printf "%q" "$i")")
+  cmd=("${cmd[@]}" "$(printf '%q' "$i")")
 done
 exe="$(mktemp /tmp/swayexec-XXXXXXXX.sh)"
 chmod +x "$exe"
-printf "#!/bin/sh\n%s\nrm %s" "${cmd[*]}" "$exe"> "$exe"
+printf '#!/bin/sh\n%s &> %s.log\n' "${cmd[*]}" "$exe" > "$exe"
 swaymsg exec -- "$exe"
