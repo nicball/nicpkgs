64c20147068a1e635909e7a60dc9729f801debee
Nick Ballard <znhihgiasy@gmail.com>
2025-03-20 13:44:29 +0800

    Update opencl-benchmark.

--- pkgs/opencl-benchmark/default.nix
+++ pkgs/opencl-benchmark/default.nix
@@ -14,16 +14,14 @@ in
 
 stdenv.mkDerivation rec {
   pname = "OpenCL-Benchmark";
-  version = "unstable-2025-02-11";
+  version = "unstable-2025-03-18";
   src = fetchFromGitHub {
     owner = "ProjectPhysX";
     repo = "OpenCL-Benchmark";
-    rev = "c98008254c0ae7ff4c288614ea70cfaa7db6d3ed";
-    sha256 = "sha256-tZ6yFs5sG1SwNIncTLW/qw2VfwC3azCft+df4naC6P4=";
+    rev = "3a6afef768dcf1435bc9a6def55d3827e1b4c11e";
+    sha256 = "sha256-6hMjhlLoFOTocYuifdor7oZVlhFGuAO/2MoB+O5G9CY=";
   };
-  buildInputs = [ clhpp ]
-    ++ lib.optionals stdenv.isDarwin [ OpenCL ]
-    ++ lib.optionals (!stdenv.isDarwin) [ opencl-headers ocl-icd ];
+  buildInputs = [ clhpp opencl-headers ocl-icd ];
   buildPhase = "g++ src/*.cpp -std=c++17 -pthread -O2 -lOpenCL -o OpenCL-Benchmark";
   installPhase = "mkdir $out/bin -p; mv OpenCL-Benchmark $out/bin";
 }
