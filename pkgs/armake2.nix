196885b7ece52cde8353ce0ff7a1703afcc2637f
Nick Ballard <znhihgiasy@gmail.com>
2025-05-03 01:27:27 +0800

    Mark cargo v1 projects as broken until nixpkgs fixes it.

--- pkgs/armake2.nix
+++ pkgs/armake2.nix
@@ -13,8 +13,9 @@ rustPlatform.buildRustPackage rec {
     rev = "2e66c9243ab08666a5689a5344517b5ddf9f8abe";
     sha256 = "sha256-gLvizbiKWsE2bpGLnSoLbqfm1cQUcnqS4l48GeF/W8k=";
   };
-  cargoHash = "sha256-Y62c74xlpveietd+zP8uo14Q0A6h9fFdnPzCYf9RBO8=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
   buildInputs = [ openssl_1_1 ];
   nativeBuildInputs = [ pkg-config ];
-  # cargoPatches = [ ./armake2.patch ];
+  meta.broken = true;
 }
