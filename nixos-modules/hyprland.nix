2f1b7240b56722cc62ddb18e6c753f6e33042089
Nick Ballard <znhihgiasy@gmail.com>
2026-02-27 16:22:58 +0800

    Use uwsm for hyprland.

--- nixos-modules/hyprland.nix
+++ nixos-modules/hyprland.nix
@@ -12,21 +12,26 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    nic.greetd.auto-login.start-command = "hyprland";
+    nic.greetd.auto-login.start-command = "uwsm start hyprland-uwsm.desktop";
     nic.waybar.wm = "hyprland";
-    programs.hyprland.enable = true;
-    systemd.packages = [ pkgs.hyprpolkitagent ];
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+    };
+    systemd = {
+      packages = with pkgs; [ hyprpolkitagent hyprpaper ];
+      user.services = {
+        hyprpolkitagent.wantedBy = [ "graphical-session.target" ];
+        hyprpaper.wantedBy = [ "graphical-session.target" ];
+      };
+    };
     environment.etc."xdg/hypr/land".source = with config.nic.window-managers; pkgs.replaceVars ./hyprland-config {
       sourceXrdb = lib.optionalString x-resources.enable ''exec-once = ${pkgs.xorg.xrdb}/bin/xrdb ${x-resources.source}'';
-      cursorTheme = cursor-theme;
-      cursorSize = cursor-size;
       inherit browser;
-      hyprpaper = "${pkgs.hyprpaper}/bin/hyprpaper";
       swaylock = "${pkgs.swaylock}/bin/swaylock";
       playerctl = "${pkgs.playerctl}/bin/playerctl";
       wpctl = "${pkgs.wireplumber}/bin/wpctl";
       screenshot = "${pkgs.screenshot}/bin/screenshot";
-      dex = "${pkgs.dex}/bin/dex";
       # keep these verbatim because '@' conflicts with replace
       DEFAULT_AUDIO_SINK = null;
       DEFAULT_AUDIO_SOURCE = null;
