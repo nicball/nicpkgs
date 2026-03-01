a16d6015bf063c61bf1f0ed4759409ef36a11438
Nick Ballard <znhihgiasy@gmail.com>
2026-03-01 20:23:43 +0800

    Add hypridle for hyprland.

--- nixos-modules/hyprland.nix
+++ nixos-modules/hyprland.nix
@@ -1,7 +1,16 @@
 { lib, config, pkgs, ... }:
 
 let
+
   cfg = config.nic.window-managers.hyprland;
+
+  graphical-session-packages = packages: {
+    packages = lib.forEach packages (name: pkgs.${name});
+    user.services = builtins.listToAttrs (lib.forEach packages (name: lib.nameValuePair name {
+      wantedBy = [ "graphical-session.target" ];
+    }));
+  };
+
 in
 
 {
@@ -18,29 +27,92 @@ in
       enable = true;
       withUWSM = true;
     };
-    systemd = {
-      packages = with pkgs; [ hyprpolkitagent hyprpaper ];
-      user.services = {
-        hyprpolkitagent.wantedBy = [ "graphical-session.target" ];
-        hyprpaper.wantedBy = [ "graphical-session.target" ];
+    environment.systemPackages = with pkgs; [ hyprlock ];
+    systemd = graphical-session-packages [ "hyprpolkitagent" "hyprpaper" "hypridle" ];
+    environment.etc = {
+      "xdg/hypr/hyprland.conf".source = with config.nic.window-managers; pkgs.replaceVars ./hyprland-config {
+        sourceXrdb = lib.optionalString x-resources.enable ''exec-once = ${pkgs.xrdb}/bin/xrdb ${x-resources.source}'';
+        inherit browser;
+        playerctl = "${pkgs.playerctl}/bin/playerctl";
+        wpctl = "${pkgs.wireplumber}/bin/wpctl";
+        screenshot = "${pkgs.screenshot}/bin/screenshot";
+        # keep these verbatim because '@' conflicts with replace
+        DEFAULT_AUDIO_SINK = null;
+        DEFAULT_AUDIO_SOURCE = null;
       };
-    };
-    environment.etc."xdg/hypr/land".source = with config.nic.window-managers; pkgs.replaceVars ./hyprland-config {
-      sourceXrdb = lib.optionalString x-resources.enable ''exec-once = ${pkgs.xrdb}/bin/xrdb ${x-resources.source}'';
-      inherit browser;
-      swaylock = "${pkgs.swaylock}/bin/swaylock";
-      playerctl = "${pkgs.playerctl}/bin/playerctl";
-      wpctl = "${pkgs.wireplumber}/bin/wpctl";
-      screenshot = "${pkgs.screenshot}/bin/screenshot";
-      # keep these verbatim because '@' conflicts with replace
-      DEFAULT_AUDIO_SINK = null;
-      DEFAULT_AUDIO_SOURCE = null;
-    };
-    environment.etc."xdg/hypr/paper" = {
-      enable = config.nic.window-managers.wallpaper.enable;
-      source = with config.nic.window-managers; pkgs.replaceVars ./hyprpaper-config {
-        wallpaper = "${wallpaper.source}";
+      "xdg/hypr/hyprpaper.conf" = {
+        enable = config.nic.window-managers.wallpaper.enable;
+        text = with config.nic.window-managers; ''
+          wallpaper {
+            monitor =
+            path = ${wallpaper.source}
+          }
+        '';
       };
+      "xdg/hypr/hyprlock.conf".text = with config.nic.window-managers; ''
+        $font = monospace
+        animations {
+            enabled = true
+            bezier = linear, 1, 1, 0, 0
+            animation = fadeIn, 1, 5, linear
+            animation = fadeOut, 1, 5, linear
+            animation = inputFieldDots, 1, 2, linear
+        }
+        ${lib.optionalString wallpaper.enable ''
+        background {
+            monitor =
+            path = ${wallpaper.source}
+            blur_passes = 3
+        }
+        ''}
+        input-field {
+          monitor =
+          size = 20%, 5%
+          outline_thickness = 3
+          inner_color = rgba(0, 0, 0, 0.0) # no fill
+          outer_color = rgba(33ccffee) rgba(00ff99ee) 45deg
+          check_color = rgba(00ff99ee) rgba(ff6633ee) 120deg
+          fail_color = rgba(ff6633ee) rgba(ff0066ee) 40deg
+          font_color = rgb(143, 143, 143)
+          fade_on_empty = false
+          rounding = 15
+          font_family = $font
+          placeholder_text = Input password...
+          fail_text = $PAMFAIL
+          dots_spacing = 0.3
+          position = 0, -20
+          halign = center
+          valign = center
+        }
+        label { # TIME
+            monitor =
+            text = $TIME
+            font_size = 90
+            font_family = $font
+            position = -30, 0
+            halign = right
+            valign = top
+        }
+        label { # DATE
+            monitor =
+            text = cmd[update:60000] date +"%A, %d %B %Y"
+            font_size = 25
+            font_family = $font
+            position = -30, -150
+            halign = right
+            valign = top
+        }
+      '';
+      "xdg/hypr/hypridle.conf".text = ''
+        general {
+          lock_cmd = pidof hyprlock || hyprlock
+          before_sleep_cmd = loginctl lock-session
+        }
+        listener {
+          timeout = 300
+          on-timeout = loginctl lock-session
+        }
+      '';
     };
     nic.backlight.enable = true;
   };
