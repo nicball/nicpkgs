ceb90ec7dd40ff8e5f3abaf21fa23ac85f1a7f62
Nick Ballard <znhihgiasy@gmail.com>
2026-08-14 17:54:56 +0800

    Unmark matrix-qq as broken.

--- pkgs/matrix-qq.nix
+++ pkgs/matrix-qq.nix
@@ -4,6 +4,5 @@ buildGoModule {
   inherit (nv-sources.matrix-qq) pname version src;
   buildInputs = [ olm ];
   vendorHash = "sha256-VLlW178B1a2mEwEb/aLZFYPVdC9hzQzI1gen+M8pg1I=";
-  meta.broken = true; # libolm
 }
 
