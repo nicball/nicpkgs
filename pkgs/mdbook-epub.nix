ca8ee287e9f5d1f1393ebab52b5c9573479092c3
Nick Ballard <znhihgiasy@gmail.com>
2026-08-31 12:08:31 +0800

    Fix mdbook-epub.

--- pkgs/mdbook-epub.nix
+++ pkgs/mdbook-epub.nix
@@ -1,10 +1,11 @@
 { nv-sources
-, rustPlatform
+, stdenv
 }:
+
+with (builtins.getFlake "github:NixOS/nixpkgs/e8be7818e19ada32105a8af937a6a473b38167ca").legacyPackages.${stdenv.system};
 
 rustPlatform.buildRustPackage {
   inherit (nv-sources.mdbook-epub) pname version src;
   doCheck = false;
-  cargoHash = "sha256-2R+CpQnSupn4oD5XnCWNgvmkg7+IHaDtOBvU7n40ix4=";
   cargoLock = nv-sources.mdbook-epub.cargoLock."Cargo.lock";
 }
